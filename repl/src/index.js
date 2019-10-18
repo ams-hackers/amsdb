47cfa13f2b6f37247389ae6c545ca9cc9cf8f444
David Vázquez Púa <davazp@gmail.com>
2019-10-18 15:08:35 +0200

    Draft for a CLI client

---
+++ repl/src/index.js
@@ -0,0 +1,84 @@
+const repl = require("repl");
+const client = require("amsdb-client");
+
+const db = client.connect("http://localhost:3000");
+let tx;
+
+const handlers = {
+  BEGIN: async args => {
+    const [mode] = args;
+    switch (mode.toUpperCase()) {
+      case "READ":
+        tx = await db.readTransaction();
+        break;
+      case "WRITE":
+        tx = await db.writeTransaction();
+        break;
+      default:
+        throw new Error(`Unknown mode ${mode}`);
+    }
+    return "ok";
+  },
+  COMMIT: async () => {
+    if (tx) {
+      await tx.commit();
+      tx = null;
+    } else {
+      throw new Error(`Not inside a transaction`);
+    }
+    return "ok";
+  },
+  ROLLBACK: async () => {
+    if (tx) {
+      await tx.rollback();
+      tx = null;
+    } else {
+      throw new Error(`Not inside a transaction`);
+    }
+    return "ok";
+  },
+  GET: async args => {
+    const [key] = args;
+    const get = tx ? tx.get.bind(tx) : db.get.bind(db);
+    const value = await get(key);
+    return value;
+  },
+  PUT: async args => {
+    const [key, value] = args;
+    if (tx && tx.put) {
+      await tx.put(key, JSON.parse(value));
+    } else {
+      throw new Error(`Not inside a write transaction`);
+    }
+
+    return "ok";
+  }
+};
+
+async function evalCommand(input) {
+  const [cmd, ...args] = input.split(/\s+/);
+
+  handlers();
+
+  switch (cmd.toUpperCase()) {
+  }
+}
+
+const amsdbRepl = repl.start({
+  prompt: "> ",
+  input: process.stdin,
+  output: process.stdout,
+  eval: (input, context, filename, callback) => {
+    evalCommand(input)
+      .then(result => {
+        callback(null, result);
+      })
+      .catch(err => {
+        callback(err);
+      })
+      .finally(() => {
+        console.log({ tx });
+        amsdbRepl.setPrompt(tx ? `${tx.token}> ` : "> ");
+      });
+  }
+});
