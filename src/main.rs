6bb28b51e6d2c6aeafd042fdea2675e0ba2455c8
Tijn Kersjes <tijn@divbyzero.nl>
2020-01-22 09:38:55 +0100

    Add demo usage in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use amsdb::Database;
+
 fn main() {
-    println!("Hello, world!");
+    let mut db = Database::new();
+    db.put("foo".to_string(), "bar".to_string());
+    match db.get("foo") {
+        Some(x) => println!("Found: {}", x),
+        None => println!("Nope"),
+    }
 }
